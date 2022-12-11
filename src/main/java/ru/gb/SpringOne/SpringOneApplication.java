8dc02d0014b951069b693ece5a1a15fe7d20fefe
Dinar Gizzatulin <dinar-arsk@yandex.ru>
2022-12-11 12:46:39 +0300

    initial commit

---
+++ src/main/java/ru/gb/SpringOne/SpringOneApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.SpringOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/gb/SpringOne/SpringOneApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.SpringOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
